6adeafd0ae6c00469f6d41280b60d3efd9f4fe99
frieder-keilholz <fgk1999@outlook.de>
2024-01-22 22:18:46 +0100

    cleanup

--- host-windows/pc_win_driver.py
+++ host-windows/pc_win_driver.py
@@ -3,13 +3,9 @@ import rotatescreen
 
 def main():
     print("Start Serial")
-    s = serial.Serial(port="COM9", baudrate=9600, timeout=None)
+    s = serial.Serial(port="COM9", baudrate=9600, timeout=None) #(customize COM Port here)
     s.flush()
     current_orientation = 0
-    #device = win32.EnumDisplayDevices(None,0)
-    #print("[%d] %s (%s)"%(0,device.DeviceString,device.DeviceName))
-    #device = win32.EnumDisplayDevices(None,1)
-    #print("[%d] %s (%s)"%(1,device.DeviceString,device.DeviceName))
     screen = rotatescreen.get_secondary_displays()[0]
     print(screen)
 
@@ -19,17 +15,15 @@ def main():
             #print(mes)
             if mes == "1\r\n":
                 if current_orientation != 1:
-                    #print("change screen orientation to horizontal")
                     current_orientation = 1
                     #change screen orientation to horizontal
-                    screen.set_landscape()
+                    screen.set_landscape() #(customize here)
 
             elif mes == "2\r\n":
                 if current_orientation != 2:
-                    #print("change screen orientation to vertical")
                     current_orientation = 2
                     #change screen orientation to vertical
-                    screen.set_portrait_flipped()
+                    screen.set_portrait_flipped() #(customize here)
                     
     except KeyboardInterrupt:
         s.close()
